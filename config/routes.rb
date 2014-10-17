58239c04fac5827b57ae7574eda8b4d1bc088b0b
Eldwick <jamesjalandoni1@gmail.com>
2014-10-17 11:32:59 -0400

    Formatted user login, register, and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+
+  # get "/" => "questions#index", as: "/"
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+  get "register" => "users#new", :as => "register"
+  get "edit_account/:id" => "users#edit", :as => "edit_account"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "search" => "questions#search", :as => "search"
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
   resources :categories
 
   resources :answers
@@ -8,18 +20,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :sessions
-
-  # get "/" => "questions#index", as: "/"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "register" => "users#new", :as => "register"
-  get "search" => "questions#search", :as => "search"
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
